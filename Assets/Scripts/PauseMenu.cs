eede9bbe1749de1f5cecba27eca0329c2de51659
geovannycordero <>
2019-09-23 21:54:21 -0600

    the game is paused but the menu not appears

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(gameIsPaused) {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+    void Resume() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+
+    }
+}
