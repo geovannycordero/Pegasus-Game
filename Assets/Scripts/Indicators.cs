e174e5c719ba44bb663847155cb3f332e1d558e2
geovannycordero <>
2019-10-01 15:46:50 -0600

    logit to show the info in the correct scene

--- Assets/Scripts/Indicators.cs
+++ Assets/Scripts/Indicators.cs
@@ -5,9 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Indicators : MonoBehaviour
 {
+  public GameObject indicators;
   public GameObject livesText;
   public GameObject pointsText;
 
+  void Start() {
+    if(SceneManager.GetActiveScene().buildIndex != 0) {
+      indicators.SetActive(true);
+    }
+  }
   void Update() {
     livesText.GetComponent<UnityEngine.UI.Text>().text = "Lives: " + Player.lives;
     pointsText.GetComponent<UnityEngine.UI.Text>().text = "Points: " + Player.lightningboltsCollected;
