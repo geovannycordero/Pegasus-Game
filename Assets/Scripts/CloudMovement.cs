7bf47a5f4655f6bc246339da06af399c21e92df3
geovannycordero <28696479+geovannycordero@users.noreply.github.com>
2019-10-20 16:23:11 -0600

    Added a file to put all the configurations

--- Assets/Scripts/CloudMovement.cs
+++ Assets/Scripts/CloudMovement.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class CloudMovement : MonoBehaviour {
 	public Rigidbody2D[] clouds;
-	private float velocity = -3F;
 	//vector que contiene la posición en donde respawnean la nube que se queda fuera de escuadre
 	private Vector2 extremoDerecho = new Vector2(19f,0f);
 
 	void Start() {
-		//se rellena el vector con las clouds
 		clouds = GetComponentsInChildren<Rigidbody2D>();
-		//se les asigna velocity a cada una
-		for(int i =0; i<clouds.Length; i++)
-		clouds[i].velocity = new Vector2(velocity,0);
 	}
 
-	// Update is called once per frame
 	void Update() {
+		setCloudsSpeed();
 		//para fijarse si alguna de las clouds se sale del encuadre
-		for(int i =0; i< clouds.Length; i++) {
+		for(int i = 0; i < clouds.Length; i++) {
 			if(clouds[i].position.x <= -32f) {
 				clouds[i].MovePosition(extremoDerecho);
 			}
 		}
+	}
+
+	void setCloudsSpeed() {
+		for(int i =0; i<clouds.Length; i++)
+			clouds[i].velocity = new Vector2(Setting.cloudsSpeed,0);
 	}
 }
