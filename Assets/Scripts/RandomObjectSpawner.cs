2e7573fb51a96be69e5216781365cbc251d6e6df
geovannycordero <28696479+geovannycordero@users.noreply.github.com>
2019-10-20 14:15:24 -0600

    code refactor

--- Assets/Scripts/RandomObjectSpawner.cs
+++ Assets/Scripts/RandomObjectSpawner.cs
@@ -5,55 +5,76 @@ using UnityEngine;
 public class RandomObjectSpawner : MonoBehaviour {
   public GameObject arrow;
   public GameObject lightning;
-  public GameObject tornado;
+  public GameObject twister;
   public GameObject anemona;
 
-  private float timeBetweenArrowSpawns;
-  private float timeBetweenThunderSpawns;
-  private float timeBetweenTornadosSpawns;
+  float timeBetweenArrowSpawn;
+  float timeBetweenThunderSpawn;
+  float timeBetweenTwisterSpawn;
 
-  // Time to decrease when there is a l evel up
+  // Time to decrease when there is a level up
   public Vector2[] initialPositions;
-  public int maxPauseTimeArrow;
-  public int minPauseTimeArrow;
-  public int maxPauseTimeThunder;
-  public int minPauseTimeThunder;
-  public int initialDistanceArrowLightning;
+  int maxPauseTimeArrow = 3;
+  int minPauseTimeArrow = 6;
+  int maxPauseTimeThunder = 3;
+  int minPauseTimeThunder = 6;
+  int initialDistanceArrowLightning = 3;
+  int minPauseTimeTwister = 0;
+  int maxPauseTimeTwister = 5;
 
   void Start() {
-    int randomNumber = Random.Range(0, 2);
-    if (randomNumber == 0) {
-      timeBetweenArrowSpawns = Random.Range(minPauseTimeArrow, maxPauseTimeArrow) + initialDistanceArrowLightning;
+    if (Random.Range(0, 2) == 0) {
+      timeBetweenArrowSpawn = Random.Range(minPauseTimeArrow, maxPauseTimeArrow) + initialDistanceArrowLightning;
     } else {
-      timeBetweenThunderSpawns = Random.Range(minPauseTimeArrow, maxPauseTimeArrow) + initialDistanceArrowLightning;
+      timeBetweenThunderSpawn = Random.Range(minPauseTimeArrow, maxPauseTimeArrow) + initialDistanceArrowLightning;
     }
-
-    timeBetweenTornadosSpawns = 5f;
+    timeBetweenTwisterSpawn = Random.Range(minPauseTimeTwister, maxPauseTimeTwister) + initialDistanceArrowLightning - 1f;
   }
 
   void Update() {
-    timeBetweenArrowSpawns -= Time.deltaTime;
-    timeBetweenThunderSpawns -= Time.deltaTime;
-    timeBetweenTornadosSpawns -= Time.deltaTime;
+    timeBetweenArrowSpawn -= Time.deltaTime;
+    timeBetweenThunderSpawn -= Time.deltaTime;
+    timeBetweenTwisterSpawn -= Time.deltaTime;
 
-    if (timeBetweenArrowSpawns <= 0) {
-      int randomNumber = Random.Range(0, initialPositions.Length);
-      Instantiate(arrow, initialPositions[randomNumber], Quaternion.identity);
-      timeBetweenArrowSpawns = Random.Range(minPauseTimeArrow, maxPauseTimeArrow);
+    if (timeBetweenThunderSpawn <= 0) {
+      createThunderObject();
     }
-    else if (timeBetweenThunderSpawns <= 0) {
-      int randomNumber = Random.Range(0, initialPositions.Length);
-      Instantiate(lightning, initialPositions[randomNumber], Quaternion.identity);
-      timeBetweenThunderSpawns = Random.Range(minPauseTimeThunder, maxPauseTimeThunder);
-    } else {
-      timeBetweenArrowSpawns -= Time.deltaTime;
-      timeBetweenThunderSpawns -= Time.deltaTime;
+    if (timeBetweenArrowSpawn <= 0) {
+      if(0 < Player.lightningboltsCollected) {
+        createArrowObject();
+      }
     }
-    if ( timeBetweenTornadosSpawns <= 0 ) {
-      //Change values to parameters
-      int randomNumber = Random.Range(0, initialPositions.Length);
-      Instantiate(tornado, initialPositions[randomNumber], Quaternion.identity);
-      timeBetweenTornadosSpawns = Random.Range(0, 5);
+    if (timeBetweenTwisterSpawn <= 0) {
+      if(4 < Player.lightningboltsCollected) {
+        createTwisterObject();
+      }
     }
+  }
+
+  void createArrowObject() {
+    Instantiate(
+      arrow,
+      initialPositions[Random.Range(0, initialPositions.Length)],
+      Quaternion.identity
+    );
+    timeBetweenArrowSpawn = Random.Range(minPauseTimeArrow, maxPauseTimeArrow);
+  }
+
+  void createThunderObject(){
+    Instantiate(
+      lightning,
+      initialPositions[Random.Range(0, initialPositions.Length)],
+      Quaternion.identity
+    );
+    timeBetweenThunderSpawn = Random.Range(minPauseTimeThunder, maxPauseTimeThunder);
+  }
+
+  void createTwisterObject() {
+    Instantiate(
+      twister,
+      initialPositions[Random.Range(0, initialPositions.Length)],
+      Quaternion.identity
+    );
+    timeBetweenTwisterSpawn = Random.Range(minPauseTimeTwister, maxPauseTimeTwister);
   }
 }
