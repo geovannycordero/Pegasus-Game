85bdc790317279cdb531604c33d1dc0c341bb719
geovannycordero <28696479+geovannycordero@users.noreply.github.com>
2019-10-29 21:12:02 -0600

    Work doing on pairing by Geovanny, Andrea y Diego before the class.

--- Assets/Scripts/Indicators.cs
+++ Assets/Scripts/Indicators.cs
@@ -5,73 +5,72 @@ using UnityEngine.SceneManagement;
 
 public class Indicators : MonoBehaviour
 {
-    public GameObject indicators, pointsText, levelText, levelUpMessage;
-    public GameObject liveOneImage, liveTwoImage, liveThreeImage,
-          liveFourImage, liveFiveImage;
-    [SerializeField] private LevelScoreBar levelScoreBar;
-    public static int level=1;
-    public float secondsLevelUp;
+  public GameObject indicators, pointsText, levelText, levelUpMessage;
+  public GameObject liveOneImage, liveTwoImage, liveThreeImage,
+  liveFourImage, liveFiveImage;
+  [SerializeField] private LevelScoreBar levelScoreBar;
+  public static int level=1;
+  public float secondsLevelUp;
 
-    void Start()
+  void Start()
+  {
+    if (SceneManager.GetActiveScene().buildIndex != 0)
     {
-        if (SceneManager.GetActiveScene().buildIndex != 0)
-        {
-            indicators.SetActive(true);
-            liveOneImage.SetActive(true);
-            liveTwoImage.SetActive(true);
-            liveThreeImage.SetActive(true);
-            liveFourImage.SetActive(true);
-            liveFiveImage.SetActive(true);
-            levelUpMessage.SetActive(false);
+      indicators.SetActive(true);
+      liveOneImage.SetActive(true);
+      liveTwoImage.SetActive(true);
+      liveThreeImage.SetActive(true);
+      liveFourImage.SetActive(true);
+      liveFiveImage.SetActive(true);
+      levelUpMessage.SetActive(false);
 
-            levelScoreBar.setSize(0);
-            levelText.GetComponent<UnityEngine.UI.Text>().text = "Level:    " + level;
-
-        }
+      // levelScoreBar.setSize(0);
+      levelText.GetComponent<UnityEngine.UI.Text>().text = "Level:    " + level;
     }
-    void Update()
+  }
+  void Update()
+  {
+    //Para pasar de nivel el jugador debe haber recolectado una cierta cantidad de rayos, sin embargo,
+    //para el puntaje tambien cuenta el numero de anemoi peleados
+    int score = Player.lightningboltsCollected + Player.anemoiFights;
+    pointsText.GetComponent<UnityEngine.UI.Text>().text = "Score:    " + score;
+    if (level != Player.level)
     {
-        //Para pasar de nivel el jugador debe haber recolectado una cierta cantidad de rayos, sin embargo,
-        //para el puntaje tambien cuenta el numero de anemoi peleados
-        int score = Player.lightningboltsCollected + Player.anemoiFights;
-        pointsText.GetComponent<UnityEngine.UI.Text>().text = "Score:    " + score;
-        if (level != Player.level)
-        {
-            level = Player.level;
-            levelText.GetComponent<UnityEngine.UI.Text>().text = "Level:    " + Player.level;
-            levelUpMessage.SetActive(true);
-            StartCoroutine(LateCall());
-        }
-        float nomalized = Player.getNormalizedScore();
-        levelScoreBar.setSize(nomalized);
-        if (Player.lives == 4)
-        {
-            liveFiveImage.SetActive(false);
-        }
-        if (Player.lives == 3)
-        {
-            liveFourImage.SetActive(false);
-        }
-        if (Player.lives == 2)
-        {
-            liveThreeImage.SetActive(false);
-        }
-        if (Player.lives == 1)
-        {
-            liveTwoImage.SetActive(false);
-        }
-        if (Player.lives < 1)
-        {
-            liveOneImage.SetActive(false);
-            levelUpMessage.SetActive(false);
-        }
-
+      level = Player.level;
+      levelText.GetComponent<UnityEngine.UI.Text>().text = "Level:    " + Player.level;
+      levelUpMessage.SetActive(true);
+      StartCoroutine(LateCall());
+    }
+    float nomalized = Player.getNormalizedScore();
+    levelScoreBar.setSize(nomalized);
+    if (Player.lives == 4)
+    {
+      liveFiveImage.SetActive(false);
+    }
+    if (Player.lives == 3)
+    {
+      liveFourImage.SetActive(false);
+    }
+    if (Player.lives == 2)
+    {
+      liveThreeImage.SetActive(false);
+    }
+    if (Player.lives == 1)
+    {
+      liveTwoImage.SetActive(false);
+    }
+    if (Player.lives < 1)
+    {
+      liveOneImage.SetActive(false);
+      levelUpMessage.SetActive(false);
     }
 
-    IEnumerator LateCall()
-    {
-        yield return new WaitForSeconds(secondsLevelUp);
-        levelUpMessage.SetActive(false);
+  }
 
-    }
+  IEnumerator LateCall()
+  {
+    yield return new WaitForSeconds(secondsLevelUp);
+    levelUpMessage.SetActive(false);
+
+  }
 }
--- Assets/Scripts/LevelScoreBar.cs
+++ Assets/Scripts/LevelScoreBar.cs
@@ -1,26 +1,25 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelScoreBar : MonoBehaviour
 {
 
-    private Transform bar;
-    // Start is called before the first frame update
-    void Start()
-    {
-        bar = transform.Find("Bar");
-        
-    }
+  private Transform bar;
+  // Start is called before the first frame update
+  void Start()
+  {
+    bar = transform.Find("Bar");
+  }
 
-    public void setSize(float sizeNormalized)
-    {
-        bar.localScale = new Vector3(sizeNormalized, 1f);
-    }
+  public void setSize(float sizeNormalized)
+  {
+    bar.localScale = new Vector3(sizeNormalized, 1f);
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
