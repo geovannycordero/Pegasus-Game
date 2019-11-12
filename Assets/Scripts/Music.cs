a40cf76dd5574a1f12bc6e3f9ffaf76512a26c82
geovannycordero <28696479+geovannycordero@users.noreply.github.com>
2019-11-12 14:36:26 -0600

    menus updated

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -1,25 +1,25 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Music : MonoBehaviour
 {
-	
+
 	/*** Para el control de la musica de fondo ***/
 	private AudioSource music;
-    // Start is called before the first frame update
-    void Start()
-    {
-        music = GetComponent<AudioSource> ();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+	void Start()
+	{
+		music = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
 		// interruptor para la musica
-        if(Input.GetKeyDown(KeyCode.M))
+		if(Input.GetKeyDown(KeyCode.M))
 		{
 			music.mute = !music.mute;
 		}
-    }
+	}
 }
